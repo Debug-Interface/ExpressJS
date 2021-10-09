717389fe9a7488d4fd4faeb2e514b9df2ab3c6d5
Brian Leyva <55462869+xxSShadow@users.noreply.github.com>
2021-10-09 12:09:48 -0700

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,22 @@
+Discord = require('discord.js');
+Client = new Discord.Client();
+Client.on('ready', () => { console.log('Ready!') })
+Client.login('TOKEN');
+
+Express = require('express');
+App = Express();
+Path = require('path');
+Parser = require('body-parser')
+
+App.use(Parser.urlencoded({extended: true}));
+App.set('views', Path.join(__dirname, '/Website'));
+App.set('view engine', 'ejs')
+App.use(Express.static('public'));
+
+App.get('/', (req, res) => {
+  res.render('Home', {Client: Client})
+});
+
+App.listen(process.env.PORT, () => {
+  console.log('Server Started')
+});
