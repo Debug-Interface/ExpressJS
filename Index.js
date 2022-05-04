1dda02d385cb8514a448962f6913aa1fdb32e144
Brian Leyva <55462869+xxSShadow@users.noreply.github.com>
2022-05-04 13:31:37 -0700

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,22 +1,22 @@
-Discord = require('discord.js');
-Client = new Discord.Client({ intents: new Discord.Intents(32767) });
-Client.on('ready', () => { console.log('Ready!') })
+const { Client, Intents } = require('discord.js');
+Client = new Client({ intents: new Intents(32767) });
+Client.once('ready', () => { console.log('Ready!') })
 Client.login('TOKEN');
 
-Express = require('express');
-App = Express();
-Path = require('path');
-Parser = require('body-parser')
+const Express = require('express');
+const App = Express();
+const Path = require('path');
+const Parser = require('body-parser');
 
-App.use(Parser.urlencoded({extended: true}));
+App.use(Parser.urlencoded({ extended: true }));
 App.set('views', Path.join(__dirname, '/Website'));
-App.set('view engine', 'ejs')
+App.set('view engine', 'ejs');
 App.use(Express.static('public'));
 
 App.get('/', (req, res) => {
-  res.render('Home', {Client: Client})
+  res.render('Home', { client: client });
 });
 
 App.listen(process.env.PORT, () => {
-  console.log('Server Started')
+  console.log('Server Started');
 });
